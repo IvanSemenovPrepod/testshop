ea67448bf08b99058be881d3f4f15bce5bd6a66a
IvanSemenovTeacher <40857580+IvanSemenovTeacher@users.noreply.github.com>
2021-01-21 14:49:57 +0300

    Добавьте файлы проекта.

---
+++ WebApplication13/Models/Items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication13.Models
+{
+    public class items
+    {
+        public int Id { get; set; }
+        public string name_tovar { get; set; }
+        public int price_tovar { get; set; }
+        public string photo { get; set; }
+
+    }
+}
---
+++ WebApplication13/Models/codesecond.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication13.Models
+{
+    public class codesecond:DbContext
+    {
+        public DbSet<items> items { get; set; }
+
+        public codesecond(DbContextOptions<codesecond> options)
+           : base(options)
+        {
+            
+        }
+    }
+}
